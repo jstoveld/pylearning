7f4b3698ade88dedba9626b124b83d0739ed85f1
John Stoveld <jstoveld@gmail.com>
2021-02-24 17:44:32 -0500

    update

--- pylearning2/workouts/guess_game2.py
+++ pylearning2/workouts/guess_game2.py
@@ -3,26 +3,24 @@ import random
 
 def guessing_game():
     answer = random.randint(0, 100)
-    
+    too_high = 101
+    too_low = -1
+
     while True:
-        try:
-            guess = int(input('What is your guess? '))
-            
-            if guess <= 0:
-                guess = int(input("Please type a positive integer?: "))
-            elif guess >= 101:
-                guess = int(input("Please type a positive integer less than 100?: "))
-            else:
-                raise ValueError
-        except ValueError:
+        guess = int(input('What is your guess? '))
+        if ValueError:
             guess = int(input('Guess must be a positive integer? '))
+
             
+        if guess < too_high and guess > too_low:
+            continue
+
         if guess is answer:
             print(f'Right!  The answer is {guess}')
             break
 
         elif guess < answer:
-           print(f'Your guess of {guess} is too low!')
+            print(f'Your guess of {guess} is too low!')
         elif guess > answer:
             print(f'Your guess of {guess} is too high!')
             
